e5f67971b9a0becaab2ee294e33ea599c2421f3b
hyungkuk <im06@naver.com>
2024-01-31 00:02:19 +0900

    feat: Ch11

--- Source/StudyProject/Private/UI/STitleLevelUI.cpp
+++ Source/StudyProject/Private/UI/STitleLevelUI.cpp
@@ -21,15 +21,15 @@ void USTitleLevelUI::NativeConstruct()
 	SavedGameButton.Get()->OnClicked.AddDynamic(this, &ThisClass::OnSavedGameButtonClicked);
 
 	SavedGameButton->SetIsEnabled(false);
-	const ASPlayerState* PS = GetDefault<ASPlayerState>();
-	if (true == ::IsValid(PS))
-	{
-		USPlayerStateSave* PlayerStateSave = Cast<USPlayerStateSave>(UGameplayStatics::LoadGameFromSlot(PS->SaveSlotName, 0));
-		if (true == ::IsValid(PlayerStateSave))
-		{
-			SavedGameButton->SetIsEnabled(true);
-		}
-	}
+	//const ASPlayerState* PS = GetDefault<ASPlayerState>();
+	//if (true == ::IsValid(PS))
+	//{
+	//	USPlayerStateSave* PlayerStateSave = Cast<USPlayerStateSave>(UGameplayStatics::LoadGameFromSlot(PS->SaveSlotName, 0));
+	//	if (true == ::IsValid(PlayerStateSave))
+	//	{
+	//		SavedGameButton->SetIsEnabled(true);
+	//	}
+	//}
 }
 
 void USTitleLevelUI::OnNewGameButtonClicked()
